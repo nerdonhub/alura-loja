6205d62e90182de242bd0179f24f82067b1619f3
Gabriel R.S. Sousa <gabriel.mywork@proton.me>
2023-11-20 19:44:41 -0300

    applying strategy pattern

--- src/main/java/br/com/alura/imposto/CalculadoraDeImpostos.java
+++ src/main/java/br/com/alura/imposto/CalculadoraDeImpostos.java
@@ -5,11 +5,7 @@ import br.com.alura.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public class CalculadoraDeImpostos {
-    public BigDecimal calcular(Orcamento orcamento, TipoImposto tipoImposto) {
-        return switch (tipoImposto) {
-            case ICMS -> orcamento.getValor().multiply(new BigDecimal("0.1"));
-            case ISS -> orcamento.getValor().multiply(new BigDecimal("0.06"));
-            default -> BigDecimal.ZERO;
-        };
-    }
+    public BigDecimal calcular(Orcamento orcamento, Imposto imposto) {
+        return imposto.calcular(orcamento);
+    };
 }
--- src/main/java/br/com/alura/imposto/TipoImposto.java
+++
@@ -1,6 +0,0 @@
-package br.com.alura.imposto;
-
-public enum TipoImposto {
-    ICMS,
-    ISS
-}
