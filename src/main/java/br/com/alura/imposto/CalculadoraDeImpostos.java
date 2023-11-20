b48d7e794b3d24faa1156ec58ac32d14422f9f5f
Gabriel R.S. Sousa <gabriel.mywork@proton.me>
2023-11-20 19:58:25 -0300

    create discount calculator class

--- src/main/java/br/com/alura/imposto/CalculadoraDeImpostos.java
+++ src/main/java/br/com/alura/imposto/CalculadoraDeImpostos.java
@@ -7,5 +7,5 @@ import java.math.BigDecimal;
 public class CalculadoraDeImpostos {
     public BigDecimal calcular(Orcamento orcamento, Imposto imposto) {
         return imposto.calcular(orcamento);
-    };
+    }
 }
---
+++ src/main/java/br/com/alura/imposto/CalculadoraDesconto.java
@@ -0,0 +1,14 @@
+package br.com.alura.imposto;
+
+import br.com.alura.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraDesconto {
+    public BigDecimal calcular(Orcamento orcamento) {
+        if (orcamento.getQuantidadeItens() > 5) {
+            return orcamento.getValor().multiply(new BigDecimal("0.1"));
+        }
+        return BigDecimal.ZERO;
+    }
+}
