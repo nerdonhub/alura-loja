ed739a5388ee60fa9b13e3c1a191bc936fd40009
Gabriel R.S. Sousa <gabriel.mywork@proton.me>
2023-11-20 20:04:44 -0300

    implements >500-discount on CalculadoraDesconto.java

--- src/main/java/br/com/alura/imposto/CalculadoraDesconto.java
+++ src/main/java/br/com/alura/imposto/CalculadoraDesconto.java
@@ -9,6 +9,9 @@ public class CalculadoraDesconto {
         if (orcamento.getQuantidadeItens() > 5) {
             return orcamento.getValor().multiply(new BigDecimal("0.1"));
         }
+        if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
+            return orcamento.getValor().multiply(new BigDecimal("0.1"));
+        }
         return BigDecimal.ZERO;
     }
 }
