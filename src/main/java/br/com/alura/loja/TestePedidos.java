f8c932930f84d45ddc7fb8bc4016d687671a5920
Gabriel R.S. Sousa <gabriel.mywork@proton.me>
2023-11-21 20:18:24 -0300

    update TestePedidos

--- src/main/java/br/com/alura/loja/TestePedidos.java
+++ src/main/java/br/com/alura/loja/TestePedidos.java
@@ -1,11 +1,8 @@
 package br.com.alura.loja;
 
-import br.com.alura.loja.orcamento.Orcamento;
 import br.com.alura.loja.pedido.GeraPedido;
-import br.com.alura.loja.pedido.Pedido;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public class TestePedidos {
     public static void main(String[] args) {
