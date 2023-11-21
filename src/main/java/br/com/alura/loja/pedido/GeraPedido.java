bfffa5079cc0210bbbcb4a14ddec10638e85500d
Gabriel R.S. Sousa <gabriel.mywork@proton.me>
2023-11-21 20:17:07 -0300

    applying command handlers pattern

---
+++ src/main/java/br/com/alura/loja/pedido/GeraPedido.java
@@ -0,0 +1,23 @@
+package br.com.alura.loja.pedido;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class GeraPedido {
+    private String cliente;
+    private BigDecimal valorOrcamento;
+    private int quantidadeItens;
+
+    public GeraPedido(String cliente, BigDecimal valorOrcamento, int quantidadeItens) {
+        this.cliente = cliente;
+        this.valorOrcamento = valorOrcamento;
+        this.quantidadeItens = quantidadeItens;
+    }
+
+    public void executa() {
+        Orcamento orcamento = new Orcamento(this.valorOrcamento, this.quantidadeItens);
+        Pedido pedido = new Pedido(cliente, LocalDateTime.now(), orcamento);
+    }
+}
