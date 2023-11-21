28b7abac989f937193f797db798b9f7a669df9f4
Gabriel R.S. Sousa <gabriel.mywork@proton.me>
2023-11-21 20:51:16 -0300

    TestePedidos.java update...

--- src/main/java/br/com/alura/loja/TestePedidos.java
+++ src/main/java/br/com/alura/loja/TestePedidos.java
@@ -2,17 +2,23 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvaPedidoBanco;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class TestePedidos {
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "Gabriel";
+        BigDecimal valorOrcamento = new BigDecimal("300");
+        int quantidadeItens = Integer.parseInt("2");
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(/*dependências*/);
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                Arrays.asList(new SalvaPedidoBanco(),
+                        new EnviarEmailPedido()
+                ));
         handler.execute(gerador);
     }
 }
